e3209e3da61340e74fd5f1d206e7d23e35ac348e
Shasandeep <118264524+Shasandeep@users.noreply.github.com>
2022-12-02 14:54:55 +0530

    java.file

---
+++ Sha3.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sha.pkg3;
+
+/**
+ *
+ * @author Smile
+ */
+public class Sha3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a=10;
+        String sha = "sandeep";
+        System.out.println("value of a = " +a +S" "+ "name sha : "+sha);        ;
+        // TODO code application logic here
+    }
+    
+}
